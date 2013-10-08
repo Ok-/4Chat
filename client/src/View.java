0cadfa480018458628d78af8f05b8ce54fb906d3
Ok- <yoros@gmx.com>
2013-10-08 20:49:14 +0200

    View windows build again from scratch

--- client/src/View.java
+++ client/src/View.java
@@ -19,12 +19,15 @@ public class View extends JFrame implements ActionListener {
     public JTextField newTopicText;
     public JButton newTopicButton;
     public JButton delTopicButton;
+    public JButton subscribeButton;
+    public JList list;
     public LinkedList<ChatTab> chatTabs;
 
     public View() {
         try {
             this.chatTabs = new LinkedList<ChatTab>();
 
+            /*
             // GUI build
             //this.setPreferredSize(new Dimension(500, 400));
             this.panel = new JPanel();
@@ -75,9 +78,102 @@ public class View extends JFrame implements ActionListener {
             this.tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
             this.add(tabbedPane);
+            */
+
+            this.setPreferredSize(new Dimension(500, 400));
+            panel = new JPanel();
+            panel.setLayout(new GridBagLayout());
+            tabbedPane = new JTabbedPane();
+            GridBagConstraints gbc;
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.weightx = 1.0;
+            gbc.weighty = 1.0;
+            gbc.fill = GridBagConstraints.BOTH;
+            panel.add(tabbedPane, gbc);
+            final JPanel panel2 = new JPanel();
+            panel2.setLayout(new GridBagLayout());
+            tabbedPane.addTab("Menu", panel2);
+            subscribeButton = new JButton();
+            subscribeButton.setText("Subscribe");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.gridy = 1;
+            gbc.gridwidth = 2;
+            gbc.weighty = 1.0;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            panel2.add(subscribeButton, gbc);
+            final JScrollPane scrollPane1 = new JScrollPane();
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            gbc.gridheight = 2;
+            gbc.weightx = 1.0;
+            gbc.weighty = 1.0;
+            gbc.fill = GridBagConstraints.BOTH;
+            panel2.add(scrollPane1, gbc);
+            this.listChansModel = new DefaultListModel();
+            list = new JList(listChansModel);
+            scrollPane1.setViewportView(list);
+            final JLabel label1 = new JLabel();
+            label1.setText("Channels");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridwidth = 3;
+            gbc.weightx = 1.0;
+            gbc.anchor = GridBagConstraints.WEST;
+            panel2.add(label1, gbc);
+            final JSeparator separator1 = new JSeparator();
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 3;
+            gbc.gridwidth = 3;
+            gbc.gridheight = 5;
+            gbc.weightx = 1.0;
+            gbc.weighty = 1.0;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            panel2.add(separator1, gbc);
+            newTopicText = new JTextField();
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 9;
+            gbc.weightx = 1.0;
+            gbc.anchor = GridBagConstraints.WEST;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            panel2.add(newTopicText, gbc);
+            newTopicButton = new JButton();
+            newTopicButton.setText("Create");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.gridy = 9;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            this.newTopicButton.addActionListener(this);
+            panel2.add(newTopicButton, gbc);
+            delTopicButton = new JButton();
+            delTopicButton.setText("Delete");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 2;
+            gbc.gridy = 9;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            this.delTopicButton.addActionListener(this);
+            panel2.add(delTopicButton, gbc);
+            final JLabel label2 = new JLabel();
+            label2.setText("Create a new Channel");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 8;
+            gbc.gridwidth = 3;
+            gbc.weightx = 1.0;
+            gbc.anchor = GridBagConstraints.WEST;
+            panel2.add(label2, gbc);
+
+
+            this.add(panel);
 
             this.client = new ForumClient(this);
-            
+
             this.pack();
         } catch (RemoteException re) {
             re.printStackTrace();
